26d883e712ab3bcf93caaa38d6f799ac8331816c
EShimel <edieshimel@gmail.com>
2015-12-14 14:35:44 -0500

    creates first draft of controllers.  Needs to test them.  Creating some seed data next and then on to curl commands to test the db.

---
+++ swap_api/app/controllers/profiles_controller.rb
@@ -0,0 +1,59 @@
+#
+class ProfilesController < OpenReadController
+  before_action :set_profile, only: [:update, :destroy]
+
+  # GET /profiles
+   def index
+    if(current_user)
+      @profiles = current_user.profiles
+    else
+      @profiles = Profile.all
+    end
+    render json: @profiles
+  end
+
+  # GET /profiles/1
+  def show
+    def show
+
+    render json: current_user.profile
+  end
+  end
+
+  # POST /profiles
+  def create
+    @profile = current_user.profiles.new(profile_params) #makes this, this user's profile.
+
+    if @profile.save
+      render json: @profile, status: :created, location: @profile
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /profiles/1
+  def update
+    if @profile.update(profile_params)
+      head :no_content
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /profiles/1
+  def destroy
+    @profile.destroy
+
+    head :no_content
+  end
+
+  def set_profile
+    @profile = current_user.profiles.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :studio, :website, :user)
+  end
+
+  private :set_profile, :profile_params
+end
--- swap_api/config/routes.rb
+++ swap_api/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  post '/register' => 'auth#register'
+  # patch '/confirm' => 'auth#confirm'
+  post '/login' => 'auth#login'
+  delete '/logout/:id' => 'auth#logout'
+  resources :users, except: [:new, :edit]
+
+  resources :profiles, except: [:new, :edit]
+  resources :resources, except: [:new, :edit]
+  resources :trades, except: [:new, :edit]
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +64,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
